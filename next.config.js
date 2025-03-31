cc99a4cad50c2bebe4b55d9fedc4e1b1ff8f7823
chtipilou <90285314+chtipilou@users.noreply.github.com>
2025-03-31 10:41:32 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Enables static HTML export
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolioQL' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolioQL/' : '',
+  // Retirer ou commenter cette ligne pour permettre l'utilisation des API routes
+  // output: 'export',
+  
+  // Autres configurations existantes
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     unoptimized: true,  // Required for static export
   },
-  trailingSlash: true,  // Recommended for GitHub Pages
 }
 
 module.exports = nextConfig;
